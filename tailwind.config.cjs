ecc01e1ed0a33688069465627071220a47410034
jamesdoth <jamesdoth@gmail.com>
2023-09-06 16:35:23 -0700

    fixed styles, impl tcss animations for ground

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,30 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		backgroundImage: {
-			'grass': "url('src/images/GRASS.png')"
-		},
 		extend: {
-			colors: {
-			}
+		animation: {
+			'move-right': 'moveRight 2.15s linear infinite', // Define the Tailwind CSS animation
+		},
+		keyframes: {
+			moveRight: {
+			'0%': { backgroundPosition: '0 0' }, // Define the keyframes
+			'100%': { backgroundPosition: '192px 0' }, // Define the keyframes
+			},
+		},
 		},
 	},
 	plugins: [],
 }
+            /* @keyframes moveRight {
+                0% {
+                    background-position: 0 0;
+                }
+                100% {
+                    background-position: 192px 0;
+                }
+            }
+
+            .animated-grass {
+                animation: moveRight 2.15s linear forwards infinite;
+                animation-iteration-count: infinite;
+            } */
