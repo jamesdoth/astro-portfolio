3d7bf4e3d7942aa83565e1f98f1c1920e276ec94
jamesdoth <jamesdoth@gmail.com>
2023-09-08 14:52:59 -0700

    finished new oval animation base. todo resizing scalexy, mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,32 +6,20 @@ module.exports = {
 			boxShadow: {
       			'moon-border': 'inset -15px 0px 0px 10px #FFFFFF',
     		},
-			// animation: {
-			// 	'move-right': 'moveRight 2.15s linear infinite',
-			// 	'move-right2': 'moveRight2 12s linear infinite',
-			// 	'spin-circle': 'spinCircle 5s linear infinite',
-			// 	'spin-counter': 'spinCounter 5s linear infinite',
-			// 	'moon-rotation': 'spinCircle 5s linear infinite reverse',
-			// 	'moon-rotation2': 'spinCounter 5s linear infinite reverse',
-			// },
-			// keyframes: {
-			// 	moveRight: {
-			// 	'0%': { backgroundPosition: '0 0' },
-			// 	'100%': { backgroundPosition: '192px 0' },
-			// 	},
-			// 	moveRight2: {
-			// 	'0%': { backgroundPosition: '0 0' },
-			// 	'100%': { backgroundPosition: '768px 0' },
-			// 	},
-			// 	spinCircle: {
-			// 	'0%': { transform: 'rotate(0deg)' },
-			// 	'100%': { transform: 'rotate(360deg)' },
-			// 	},
-			// 	spinCounter: {
-			// 	'0%': { transform: 'rotate(0deg)' },
-			// 	'100%': { transform: 'rotate(-360deg)' },
-			// 	}
-			// },
+			animation: {
+				'move-right': 'moveRight 2.15s linear infinite',
+				'move-right2': 'moveRight2 12s linear infinite',
+			},
+			keyframes: {
+				moveRight: {
+				'0%': { backgroundPosition: '0 0' },
+				'100%': { backgroundPosition: '192px 0' },
+				},
+				moveRight2: {
+				'0%': { backgroundPosition: '0 0' },
+				'100%': { backgroundPosition: '768px 0' },
+				},
+			},
 		},
 	},
 	plugins: [],
@@ -59,3 +47,18 @@ module.exports = {
 			// 		transform: rotate(360deg);
 			// 	}
         	// }
+
+
+				// 'spin-circle': 'spinCircle 5s linear infinite',
+				// 'spin-counter': 'spinCounter 5s linear infinite',
+				// 'moon-rotation': 'spinCircle 5s linear infinite reverse',
+				// 'moon-rotation2': 'spinCounter 5s linear infinite reverse',
+
+								// spinCircle: {
+				// '0%': { transform: 'rotate(0deg)' },
+				// '100%': { transform: 'rotate(360deg)' },
+				// },
+				// spinCounter: {
+				// '0%': { transform: 'rotate(0deg)' },
+				// '100%': { transform: 'rotate(-360deg)' },
+				// }
